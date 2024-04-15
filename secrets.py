7b9e3fecc9f2647c68c8faa987aa2fdfe5623eff
Juan Carlos Mancera Suárez <gu22ia0014@globaluniversity.edu.mx>
2024-04-15 08:03:14 -0600

    init

---
+++ secrets.py
@@ -0,0 +1,10 @@
+# secrets.py
+
+# Datos de conexión a la base de datos
+dbhost = '107.161.75.132'
+dbuser = 'muud_videos'
+dbpass = 'muud_videos'
+dbname = 'muud_videos'
+
+# Clave secreta para proteger sesiones de usuario y otros datos sensibles
+SECRET_KEY = 'clave_secreta_aleatoria'
